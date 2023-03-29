a87f1b1c47518197c68244aa1edc09ab22f75adf
soju <underrated@gmail.com>
2023-03-29 16:17:05 -0700

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+"use client";
+/* eslint-disable @next/next/no-head-element */
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+import { configureChains, createClient, goerli, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import "../styles/globals.css";
+import "../styles/tailwind.css";
+import Navbar from "./Navbar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { chains, provider } = configureChains(
+    [goerli],
+    [
+      // alchemyProvider({
+      //   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY ?? "",
+      // }),
+      publicProvider(),
+    ]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "My RainbowKit App",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
+  return (
+    <html>
+      <head></head>
+      <body>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <div className="mx-auto">
+              <Navbar />
+              {children}
+            </div>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </body>
+    </html>
+  );
+}
